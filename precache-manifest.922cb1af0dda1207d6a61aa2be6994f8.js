496c0796551adba4e91c696d459a11556d079c39
Sergio Adame <sergioadameva@gmail.com>
2022-04-03 04:52:55 -0500

    Updates

---
+++ precache-manifest.922cb1af0dda1207d6a61aa2be6994f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d9a14d484f1cf000b66c3e5e690053",
+    "url": "/ancona/index.html"
+  },
+  {
+    "revision": "bf03e0bc970e95532d9e",
+    "url": "/ancona/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f12d851d723eceea1f4e",
+    "url": "/ancona/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bf03e0bc970e95532d9e",
+    "url": "/ancona/static/js/2.ffce8b34.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ancona/static/js/2.ffce8b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12d851d723eceea1f4e",
+    "url": "/ancona/static/js/main.841e741a.chunk.js"
+  },
+  {
+    "revision": "460ae38041babfd42d70",
+    "url": "/ancona/static/js/runtime-main.d7f1d849.js"
+  }
+]);
